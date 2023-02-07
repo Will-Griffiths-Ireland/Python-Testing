39598f4892c500ebacf4aea3e8918bc446f78611
Will-Griffiths-Ireland <williamgriffithsireland@hotmail.com>
2023-02-07 11:12:14 +0000

    Added more logic

--- run.py
+++ run.py
@@ -11,7 +11,6 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
-
 sales = SHEET.worksheet('sales')
 data = sales.get_all_values()
 
@@ -19,13 +18,16 @@ def get_sales_data():
     """
     Get sales figures from user
     """
-    print('Please input sales figures')
-    print('6 numbers separated by commas\n')
-    data_str = input('Enter your data here: ')
-    print(f'The data provided is {data_str}')
-    sales_data = data_str.split(',')
-    validate_data(sales_data)
-
+    while True:
+        print('Please input sales figures')
+        print('6 numbers separated by commas\n')
+        data_str = input('Enter your data here: ')
+        print(f'The data provided is {data_str}')
+        sales_data = data_str.split(',')
+        if validate_data(sales_data):
+            print('Data is valid!')
+            break
+    return sales_data        
 
 
 def validate_data(values):
@@ -41,5 +43,24 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f'Invalid data: {e}, try again bozo. \n')
+        return False
 
-get_sales_data()
+    return True
+
+
+
+
+def update_sales_worksheet(data):
+    """
+        Update sales worksheet with a new line and data provided
+    """
+    print('Updating sales worksheet.../n')
+    sales_worksheet = SHEET.worksheet('sales')
+    sales_worksheet.append_row(data)
+    print('Sales worksheet updated successfully!/n')
+
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+print(sales_data)
+update_sales_worksheet(sales_data)    
