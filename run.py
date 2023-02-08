f39e81e05f963535bb7fdfaff3a5bce2224f74fd
Will-Griffiths-Ireland <williamgriffithsireland@hotmail.com>
2023-02-08 12:19:38 +0000

    Testing

--- run.py
+++ run.py
@@ -1,66 +1,50 @@
-import gspread
-from google.oauth2.service_account import Credentials
+from rich.console import Console
+from rich.panel import Panel
+import os
+from rich import print
+import time
+os.system('cls' if os.name == 'nt' else 'clear')
+c = Console()
+f = open('logo.txt')
+logo = f.read()
+i=0
+line = ''
+for i in range(39):
+    line += ' ✩'
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+index = 8
+color = 'rgb('+ str(index) +','+ str(index) +',' + str(index) + ')'
+while True:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    c.print(line +'\n', style='yellow')
+    c.print(logo, style = color)
+    c.print(line +'\n', style='yellow')
+    c.print('           A STAR IS A FIXED POINT OF LIGHT BY WHICH TO GUIDE US')
+    c.print('           EVERY EVENT TAKES PLACES IN A SINGULAR MOMENT IN SPACE')
+    c.print('           THE MANIFEST IS FINAL......')
+    print('\n')
+    time.sleep(.1)
+    index += 10
+    color = 'rgb('+ str(index) +','+ str(index) +',' + str(index) + ')'
+    if index > 240:
+        break
 
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('love_sandwiches')
-sales = SHEET.worksheet('sales')
-data = sales.get_all_values()
+UERI = 'HGY6-GSRF-534R-H7DS'
+name = input('Whats your name? : \n')
+print('\n')
+c.clear()
+while len(name) < 20:
+    name += ' '
 
-def get_sales_data():
-    """
-    Get sales figures from user
-    """
-    while True:
-        print('Please input sales figures')
-        print('6 numbers separated by commas\n')
-        data_str = input('Enter your data here: ')
-        print(f'The data provided is {data_str}')
-        sales_data = data_str.split(',')
-        if validate_data(sales_data):
-            print('Data is valid!')
-            break
-    return sales_data        
-
-
-def validate_data(values):
-    """
-    validate data
-    """
-    print(values)
-    try:
-        [int(value) for value in values]
-        if len(values) != 6:
-            raise ValueError(
-                f'I said you need 6 numbers! You gave {len(values)}'
-            )
-    except ValueError as e:
-        print(f'Invalid data: {e}, try again bozo. \n')
-        return False
-
-    return True
-
+c.print(f'╔════════[ Passport ]═════════╗ ╔═════════════════════════════╗', style="white on blue")
+c.print(f'║                             ║ ║                             ║', style="white on blue")
+c.print(f'║ Name: {name}  ║ ║ Name: {name}  ║' , style="white on blue")
+c.print(f'║ UERI: {UERI}   ║ ║                             ║', style="white on blue")
+c.print(f'║                             ║ ║                             ║', style="white on blue")
+c.print(f'║                             ║ ║                             ║', style="white on blue")
+c.print(f'║                             ║ ║                             ║', style="white on blue")
+c.print(f'║                             ║ ║                             ║', style="white on blue")
+c.print(f'╚═════════════════════════════╝ ╚═════════════════════════════╝', style="white on blue")
 
 
-
-def update_sales_worksheet(data):
-    """
-        Update sales worksheet with a new line and data provided
-    """
-    print('Updating sales worksheet.../n')
-    sales_worksheet = SHEET.worksheet('sales')
-    sales_worksheet.append_row(data)
-    print('Sales worksheet updated successfully!/n')
-
-
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-print(sales_data)
-update_sales_worksheet(sales_data)    
+# print('╚╝╬═╩╠╣╦╔╗║')
